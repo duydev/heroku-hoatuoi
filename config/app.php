f04cc1d27d697c3752e36bf9ca949b6374648516
Khải Hoàn Lê <Khi Hon L>
2016-11-30 22:35:31 +0700

    Sua app config

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://hoatuoi-uphost.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
